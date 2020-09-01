f10b9140f0b4669b050552f2fe033bb4452b3695
nick-sib <khc1981@mail.ru>
2020-09-01 11:00:12 +0700

    2. Сконфигурировать Kotlin в новом проекте (лучше вручную). 3. Перевести MainActivity на Kotlin.

--- app/src/main/java/com/nickolay/kotlin_for_android/MainActivity.kt
+++ app/src/main/java/com/nickolay/kotlin_for_android/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.nickolay.kotlin_for_android;
+package com.nickolay.kotlin_for_android
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
