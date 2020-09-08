2f5ccc11f2c3e8abc4f7e71daaeaee30ce7451c5
nick-sib <khc1981@mail.ru>
2020-09-08 16:53:59 +0700

    6. Добавить функционал создания новой заметки.

---
+++ app/src/main/java/com/nickolay/kotlin_for_android/ui/note/NoteViewModel.kt
@@ -0,0 +1,20 @@
+package com.nickolay.kotlin_for_android.ui.note
+
+import androidx.lifecycle.ViewModel
+import com.nickolay.kotlin_for_android.data.NotesRepository
+import com.nickolay.kotlin_for_android.data.entity.Note
+
+class NoteViewModel(private val repository: NotesRepository = NotesRepository) : ViewModel() {
+    private var pendingNote: Note? = null
+
+    fun saveChanges(note: Note) {
+        pendingNote = note
+    }
+
+    override fun onCleared() {
+//        if (pendingNote != null) {
+//            repository.saveNote(pendingNote!!)
+//        }
+    }
+
+}
