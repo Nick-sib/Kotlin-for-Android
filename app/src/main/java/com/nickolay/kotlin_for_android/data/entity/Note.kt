39cad7a76c0450606205e34525a22919bf817072
nick-sib <khc1981@mail.ru>
2020-09-08 14:39:31 +0700

    Изменения сделаные вовремя урока

--- app/src/main/java/com/nickolay/kotlin_for_android/data/entity/Note.kt
+++ app/src/main/java/com/nickolay/kotlin_for_android/data/entity/Note.kt
@@ -1,3 +1,32 @@
 package com.nickolay.kotlin_for_android.data.entity
 
-data class Note(var title: String, var text: String, var color: Int)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+data class Note(
+        val id: String,
+        var title: String,
+        var text: String,
+        var color: Int,
+        var lastChanged: Date = Date()): Parcelable {
+
+
+    override fun equals(other: Any?) =
+        when{
+            (this === other) -> true
+            (javaClass != other?.javaClass) -> false
+            (id != (other as Note).id) -> false
+            else -> true
+        }
+
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + text.hashCode()
+        result = 31 * result + color
+        return result
+    }
+}
