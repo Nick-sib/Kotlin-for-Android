53534d8428f2df31819eedd4852e3fcc6fe122ed
nick-sib <khc1981@mail.ru>
2020-09-17 22:28:05 +0700

    Заголовок в одну строчку

--- app/src/main/java/com/nickolay/kotlin_for_android/ui/note/NoteActivity.kt
+++ app/src/main/java/com/nickolay/kotlin_for_android/ui/note/NoteActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.MenuItem
-import android.widget.EditText
 import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.ViewModelProvider
 import com.nickolay.kotlin_for_android.R
