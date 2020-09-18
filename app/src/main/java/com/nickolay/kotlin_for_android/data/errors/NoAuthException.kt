b459aa61f5c7730a4d2807d66cb843d3920cfe66
nick-sib <khc1981@mail.ru>
2020-09-18 17:04:28 +0700

    Add user provider

---
+++ app/src/main/java/com/nickolay/kotlin_for_android/data/errors/NoAuthException.kt
@@ -0,0 +1,3 @@
+package com.nickolay.kotlin_for_android.data.errors
+
+class NoAuthException(msg: String? = null) : Throwable(msg)
--- app/src/main/java/com/nickolay/kotlin_for_android/data/provider/DataProvider.kt
+++ app/src/main/java/com/nickolay/kotlin_for_android/data/provider/DataProvider.kt
@@ -2,10 +2,12 @@ package com.nickolay.kotlin_for_android.data.provider
 
 import androidx.lifecycle.LiveData
 import com.nickolay.kotlin_for_android.data.entity.Note
+import com.nickolay.kotlin_for_android.data.entity.User
 import com.nickolay.kotlin_for_android.data.model.NoteResult
 
 interface DataProvider {
 
+    fun getCurrentUser(): LiveData<User?>
     fun subscribeToAllNotes() : LiveData<NoteResult>
     fun saveNote(note: Note) : LiveData<NoteResult>
     fun getNoteByID(id: String) : LiveData<NoteResult>
