927cb51e4bb43925101b719dbcb7c76b25333742
nick-sib <khc1981@mail.ru>
2020-09-29 16:52:38 +0700

    deleted other not used ..ViewState.kt

--- app/src/main/java/com/nickolay/kotlin_for_android/ui/splash/SplashViewState.kt
+++
@@ -1,5 +0,0 @@
-package com.nickolay.kotlin_for_android.ui.splash
-
-import com.nickolay.kotlin_for_android.ui.base.BaseViewState
-
-class SplashViewState (authenticated: Boolean? = null, error: Throwable? = null) : BaseViewState<Boolean?>(authenticated, error)
