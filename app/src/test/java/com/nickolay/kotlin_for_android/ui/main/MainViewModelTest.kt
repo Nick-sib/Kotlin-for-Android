5d622126bf882f2fb98b6c2655e120b4391868bb
nick-sib <khc1981@mail.ru>
2020-09-29 22:03:46 +0700

    4.*Переписать тесты с использованием корутин MainViewModelTest

--- app/src/test/java/com/nickolay/kotlin_for_android/ui/main/MainViewModelTest.kt
+++ app/src/test/java/com/nickolay/kotlin_for_android/ui/main/MainViewModelTest.kt
@@ -1,7 +1,6 @@
 package com.nickolay.kotlin_for_android.ui.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
 import com.nickolay.kotlin_for_android.data.NotesRepository
 import com.nickolay.kotlin_for_android.data.entity.Note
 import com.nickolay.kotlin_for_android.data.model.NoteResult
@@ -9,6 +8,9 @@ import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -20,55 +22,49 @@ class MainViewModelTest{
     val taskExecutionRule = InstantTaskExecutorRule()
 
     private val mockRepository = mockk<NotesRepository>()
-    private val notesLiveData = MutableLiveData<NoteResult>()
+    private val channel = Channel<NoteResult>(Channel.CONFLATED)
     private lateinit var viewModel: MainViewModel
 
-//    @Before
-//    fun setup(){
-//        clearAllMocks()
-//        every { mockRepository.getNotes() } returns notesLiveData
-//        viewModel = MainViewModel(mockRepository)
-//    }
-//
-//    @Test
-//    fun `should call getNotes once`() {
-//        verify(exactly = 1) { mockRepository.getNotes() }
-//    }
-//
-//    @Test
-//    fun `should return Notes`() {
-//        var result: MainViewState? = null
-//        val testData = listOf(Note(), Note())
-//
-//        viewModel.getViewState().observeForever{
-//            result = it
-//        }
-//        notesLiveData.value = NoteResult.Success(testData)
-//
-//        assertEquals(result?.data, testData)
-//        assertEquals(result?.error, null)
-//    }
-//
-//    @Test
-//    fun `should return error`() {
-//        var result: MainViewState? = null
-//        val testData = Throwable("error")
-//
-//        viewModel.getViewState().observeForever{
-//            result = it
-//        }
-//        notesLiveData.value = NoteResult.Error(testData)
-//
-//        assertEquals(result?.error, testData)
-//        assertEquals(result?.notes, null)
-//
-//    }
-//
-//    @Test
-//    fun `should remove observer`(){
-//        viewModel.onCleared()
-//
-//        assertFalse(notesLiveData.hasObservers())
-//    }
+    @Before
+    fun setup(){
+        clearAllMocks()
+        every { (mockRepository.getNotes())} returns channel
+        viewModel = MainViewModel(mockRepository)
+    }
+
+    @Test
+    fun `should call getNotes once`() {
+        verify(exactly = 1) { mockRepository.getNotes() }
+    }
+
+    @Test
+    fun `should return Notes`() = runBlocking{
+        val testData = listOf(Note(), Note())
+
+        val deferred = async {
+            viewModel.getViewState().receive()
+        }
+        channel.send(NoteResult.Success(testData))
+
+        assertEquals(deferred.await(), testData)
+    }
+
+    @Test
+    fun `should return error`() = runBlocking {
+        val testData = Throwable("error")
+        val deferred = async {
+            viewModel.getErrorChannel().receive()
+        }
+        channel.send(NoteResult.Error(testData))
+        assertEquals(deferred.await(), testData)
+    }
+
+    @Test
+    fun `should remove observer`() = runBlocking{
+        val testData = listOf(Note(), Note())
+        channel.send(NoteResult.Success(testData))//нополняем
+        viewModel.onCleared()                     //очищаем
+        assertFalse(channel.isFull)
+    }
 
 }
